d4a5bf82e6b00b4b5a7fbfd74368fa80214747ec
Andrey Kruglov <kruglov.it@gmail.com>
2025-04-30 21:09:55 +0300

    fix 2

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,20 +1,19 @@
-import { defineConfig } from "cypress";
-import vitePreprocessor from "cypress-vite";
+import { defineConfig } from 'cypress';
+import vitePreprocessor from 'cypress-vite';
 
 export default defineConfig({
-  e2e: {
-	baseUrl: "http://localhost:5173",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-	  on('file:preprocessor', vitePreprocessor())
-    },
-  },
+	e2e: {
+		baseUrl: 'http://localhost:5173',
+		setupNodeEvents(on) {
+			// implement node event listeners here
+			on('file:preprocessor', vitePreprocessor());
+		},
+	},
 
-
-  component: {
-	  devServer: {
-		  framework: "react",
-		  bundler: "vite",
-	  }
-  }
+	component: {
+		devServer: {
+			framework: 'react',
+			bundler: 'vite',
+		},
+	},
 });
