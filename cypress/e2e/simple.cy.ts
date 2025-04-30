84da09114d78ff23c8f6d3cd30a8136f08e81436
Andrey Kruglov <kruglov.it@gmail.com>
2025-04-30 17:03:09 +0300

    fix: cypress tests

--- cypress/e2e/simple.cy.ts
+++ cypress/e2e/simple.cy.ts
@@ -1,10 +1,7 @@
 /// <reference types="cypress" />
 
-import { add } from '@utils/utils.ts';
-
 describe('template spec', () => {
 	it('passes', () => {
 		cy.visit('/');
-		const res = add('a', 'b');
 	});
 });
